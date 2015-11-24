7e26edb51e284fc374ec4bad577a575d16d5b956
24547744m <24547744m@github.com>
2015-11-24 01:29:28 +0100

    links para agregar imagenes a listview

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -16,6 +16,11 @@ import java.util.List;
 
 public class Controller {
 
+    // iconos del tiempo openweathermap => http://openweathermap.org/weather-conditions 
+    //link para agregar imagenes al imageView http://stackoverflow.com/questions/15661500/javafx-listview-item-with-an-image-button
+    // http://docs.oracle.com/javafx/2/ui_controls/list-view.htm#CEGGEDBF http://java-buddy.blogspot.com.es/2013/05/implement-javafx-listview-for-custom.html
+
+
     private List<Time> objTimes;
 //    private Location objLocation;
     private List<Item> itemsList;//Lista de objetos de tipo -> Item(ImageView, Time)
